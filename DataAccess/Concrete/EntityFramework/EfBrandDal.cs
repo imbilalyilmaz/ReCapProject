498bbe5123186b7ed56f0524d4cdfee3ee14d981
imbilalyilmaz <bilalyilmaz.gs@gmail.com>
2023-09-22 20:00:43 +0300

    9.Gün

--- DataAccess/Concrete/EntityFramework/EfBrandDal.cs
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -11,29 +11,8 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfBrandDal : IBrandDal
     {
-        public void Add(Brand entity)
-        {
-            throw new NotImplementedException();
-        }
 
-        public void Delete(Brand entity)
-        {
-            throw new NotImplementedException();
-        }
 
-        public Brand Get(Expression<Func<Brand, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Brand> GetAll(Expression<Func<Brand, bool>> filter = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Brand entity)
-        {
-            throw new NotImplementedException();
-        }
+       
     }
 }
